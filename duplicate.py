18d0c5e359c15a8334958d251784ab0442a3409f
furkanckkmus <furkancok@gmail.com>
2024-03-31 19:00:43 +0300

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,14 +1,16 @@
-
 dosyanınicindekiler = []
 temiz_liste = []
-with open("input.txt","r")as f:
-    for satir in f:
-        satir = satir.strip()  # Satır sonundaki boşlukları temizle
-        dosyanınicindekiler.append(satir)
-temiz_liste = list(set(dosyanınicindekiler))
-temiz_liste = sorted(temiz_liste)
-with open("output.txt","w")as f:
-    for i in temiz_liste:
-        f.write(i)
-        f.write("\n")
-print("İŞLEM BİTTİ")
+try:
+    with open("input.txt","r",encoding="utf-8")as f:
+        for satir in f:
+            satir = satir.strip()  # Satır sonundaki boşlukları temizle
+            dosyanınicindekiler.append(satir)
+    temiz_liste = list(set(dosyanınicindekiler))
+    temiz_liste = sorted(temiz_liste)
+    with open("output.txt","w")as f:
+        for i in temiz_liste:
+            f.write(i)
+            f.write("\n")
+    print("İŞLEM BİTTİ")
+except:
+    print("Pass")
