40fa051becea8e8e1605c2fd8f7a6e99a775859f
furkanckkmus <furkancok@gmail.com>
2025-06-13 17:17:23 +0300

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,16 +1,17 @@
 dosyanınicindekiler = []
 temiz_liste = []
 try:
-    with open("input.txt","r",encoding="utf-8")as f:
+    with open("input.txt", "r", encoding="utf-8") as f:
         for satir in f:
             satir = satir.strip()  # Satır sonundaki boşlukları temizle
             dosyanınicindekiler.append(satir)
     temiz_liste = list(set(dosyanınicindekiler))
     temiz_liste = sorted(temiz_liste)
-    with open("output.txt","w")as f:
+    
+    with open("output.txt", "w", encoding="utf-8") as f:  # encoding parametresini ekleyerek yazma işlemi yapıyoruz
         for i in temiz_liste:
             f.write(i)
             f.write("\n")
     print("İŞLEM BİTTİ")
-except:
-    print("Pass")
+except Exception as e:
+    print(f"Hata oluştu: {e}")  # Hata mesajını yazdırıyoruz
